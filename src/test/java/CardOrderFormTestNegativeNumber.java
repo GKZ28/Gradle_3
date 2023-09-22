570a44745f102189b31b102f64796f5a46a64ff3
gkz <giorgadzekonstantin@gmail.com>
2023-09-22 13:17:28 +0300

    Added Tests

---
+++ src/test/java/CardOrderFormTestNegativeNumber.java
@@ -0,0 +1,43 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import static org.junit.Assert.*;
+
+public class CardOrderFormTestNegativeNumber {
+
+    public static void main(String[] args) {
+        // Указываем путь к драйверу браузера (Chrome)
+        System.setProperty("webdriver.chrome.driver", "D:\\Download\\chromedriver_win32(1)\\chromedriver.exe");
+
+        // Настройка для включения headless-режима
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--disable-gpu"); // Эти параметры могут варьироваться в зависимости от версии Chrome
+
+        // Инициализация WebDriver с настройками
+        WebDriver driver = new ChromeDriver(options);
+
+        // Открываем страницу
+        driver.get("http://localhost:9999");
+
+        // Заполняем поля формы
+        WebElement nameInput = driver.findElement(By.cssSelector("[data-test-id='name'] input"));
+        WebElement phoneInput = driver.findElement(By.cssSelector("[data-test-id='phone'] input"));
+        WebElement agreementCheckbox = driver.findElement(By.cssSelector("[data-test-id='agreement']"));
+        WebElement submitButton = driver.findElement(By.cssSelector("[data-test-id='submit']"));
+
+        nameInput.sendKeys("Иван Иванов");
+        phoneInput.sendKeys("+791234");  // Неполный номер телефона
+        agreementCheckbox.click();
+        submitButton.click();
+
+        // Проверяем, что сообщение об успешной отправке не отображается
+        WebElement successMessage = driver.findElement(By.cssSelector("[data-test-id='order-success']"));
+        assertTrue(!successMessage.isDisplayed());
+
+        // Закрываем браузер
+        driver.quit();
+    }
+}
